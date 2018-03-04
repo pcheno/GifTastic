7d6269242b2635e1945c3c72d7a112cb11c1212a
Paul Cheno <pwchenoweth@gmail.com>
2018-03-04 14:53:46 -0600

    my on click for my button is not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // Creates an array containing some animals
   var animals = ["Camel", "Horse", "Skunk", "Cow", "Bird", "Coyote", "Dog", "Pig", "Duck", "Opossum"]
   // Create array containing acceptable ratings
-  var ratings = ["g","pg","y"]
+  var ratings = ["g", "pg", "y"]
 
   function renderButtons() {
     //First empty out the div
@@ -21,17 +21,16 @@ $(document).ready(function () {
       $("#animalButtons").append(a);
 
     } //for loop animals
-
   } // function renderButtons
 
-  // adding an animal
+  // adding an animal on click
   $("#addAnimal").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var animal = $("#animalInput").val().trim();
 
     // Adding movie from the textbox to our array. Check to make sure it is not already there, and if it is blank
-    if (($.inArray(animal, animals) < 0)  && (animal.length >0)) {
+    if (($.inArray(animal, animals) < 0) && (animal.length > 0)) {
       animals.push(animal);
     }
     // Calling renderButtons which handles the processing of our animal array
@@ -39,8 +38,59 @@ $(document).ready(function () {
     // Clear input
     $("#animalInput").val("");
   });
+  ////new adding on animalBtn
 
+  $("button").on("click", function (event) {
+    console.log("we got here");
+    event.preventDefault();
+    // Grabbing and storing the data-animal property value from the button
+    var animal = $(this).attr("data-name");
+    console.log("we got here");
+    // Constructing a queryURL using the animal name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      // After data comes back from the request
+      .then(function (response) {
+        console.log(queryURL);
+
+        console.log(response);
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+          // Creating and storing a div tag
+          var animalDiv = $("<div>");
+
+          // Creating a paragraph tag with the result item's rating
+          var p = $("<p>").text("Rating: " + results[i].rating);
+
+          // Creating and storing an image tag
+          var animalImage = $("<img>");
+          // Setting the src attribute of the image to a property pulled off the result item
+          animalImage.attr("src", results[i].images.fixed_height.url);
+
+          // Appending the paragraph and image tag to the animalDiv
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+
+          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+          $("#animalGif").prepend(animalDiv);
+        }
+      });
+  });
+
+  ////end of on animalBtn
+
+
+  //Just beginning, nothing clicked yet
   renderButtons();
-
 
 }); //$(document).ready(function ()
