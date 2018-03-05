dab284ceb5945c362a8b974744fe98f71472f600
Paul Cheno <pwchenoweth@gmail.com>
2018-03-05 10:08:02 -0600

    working on animal buttons, using some code from class

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     for (var i = 0; i < animals.length; i++) {
 
       var a = $("<button>");
-
+     
       a.addClass("animalBtn");
       a.attr("data-name", animals[i]);
       a.text(animals[i]);
@@ -38,58 +38,62 @@ $(document).ready(function () {
     // Clear input
     $("#animalInput").val("");
   });
-  ////new adding on animalBtn
-
-  $("button").on("click", function (event) {
-    console.log("we got here");
+  
+  $("#animalBtn").on("click", function (event) {
     event.preventDefault();
-    // Grabbing and storing the data-animal property value from the button
-    var animal = $(this).attr("data-name");
     console.log("we got here");
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      // After data comes back from the request
-      .then(function (response) {
-        console.log(queryURL);
-
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
-
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
-
-          // Creating and storing a div tag
-          var animalDiv = $("<div>");
-
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
-
-          // Creating and storing an image tag
-          var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#animalGif").prepend(animalDiv);
-        }
-      });
   });
+  $(".animalBtn").delegate("a", "click",function(){
+    console.log("animal button clicked here")
 
-  ////end of on animalBtn
+   // $("a").css("background-color", "pink");
+});
+$(document).on("click", ".animalBtn", function(){
+  alert ('button clicked by doc');
+  ////add here
+  var animal = $(this).attr("data-animal");
 
+  // Constructing a queryURL using the animal name
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+  // Performing an AJAX request with the queryURL
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    // After data comes back from the request
+    .then(function(response) {
+      console.log(queryURL);
+
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.data;
+
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
+
+        // Creating and storing a div tag
+        var animalDiv = $("<div>");
+
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + results[i].rating);
+
+        // Creating and storing an image tag
+        var animalImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        animalImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and image tag to the animalDiv
+        animalDiv.append(p);
+        animalDiv.append(animalImage);
+
+        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gifs-appear-here").prepend(animalDiv);
+      }
+    });
+  ////
+});
   //Just beginning, nothing clicked yet
   renderButtons();
 
